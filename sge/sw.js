9834767f4210749b2cd3007d78f570261b645573
SGE Deploy <sge@local>
2026-09-18 18:47:55 -0400

    PWA fix: altura 100dvh respeita barra de URL do celular (nav e scroll funcionam)

--- sge/sw.js
+++ sge/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sge-pwa-v2';
+const CACHE = 'sge-pwa-v3';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './sge-logo.css', './icons/logo.png', './icons/icon-192.png', './icons/icon-512.png', './icons/logo_ad_brasil.png'];
 
 self.addEventListener('install', (e) => {
