f6721068e5e8c12cf1ff36da38e23468b6b8a83b
Carlos Daniel <sge@local>
2026-09-24 13:52:27 -0400

    v1.107 - Push de contribuicao para membro vinculado + banner de notificacao no modo membro

--- sge/sw.js
+++ sge/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sge-pwa-v114';
+const CACHE = 'sge-pwa-v115';
 const SHELL = ['./', './index.html', './gestao.js',
   './financas.js', './espiritual.js', './comunidade.js', './membro.js', './missoes.js', './manifest.webmanifest', './sge-logo.css', './icons/logo.png', './icons/icon-192.png', './icons/icon-512.png', './icons/logo_ad_brasil.png', './icons/logo_sge.png', './icons/cabecalho_ad_brasil.png'];
 
